9eb928dc0b6b65cec59a38b3852c8a0dc40a5c88
developerpanda083 <cpandalee083@gmail.com>
2022-12-09 00:49:15 -0500

    Exercise 02 done and passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, mul) {
+    let mulString = '';
+    if (mul >= 0) {
+        for (i = 1; i <= mul; i++) {
+            mulString += str;
+        }
+    } else {
+        return "ERROR"
+    }
+    return mulString
 };
 
 // Do not edit below this line
