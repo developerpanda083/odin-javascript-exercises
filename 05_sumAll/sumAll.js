89d080d4746f0774a83fa0bb50280b569ef7f629
developerpanda083 <cpandalee083@gmail.com>
2022-12-09 16:13:06 -0500

    Exercise 05 finished and passed. Commented

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+// fx has 2 inputs and returns sum of all numbers between the two inputs
+// works if 2nd input is larger than first
+// does not work if either number is negative or not a number (ie arrays, strings, etc)
+const sumAll = function(start, end) {
+    let total = 0;
+    let counter = 0;
 
+    // error handling as stated above
+    if (typeof start !== "number" || start < 0 || typeof end !== "number" || end < 0) {
+        return "ERROR"
+    } else {
+        // if 1st input is < than 2nd input
+        if (start < end) {
+            for (i = start; i <= end+1; i++) { // from input start to input end + 1
+                // add to counter to total
+                total += counter;
+                // increment counter
+                counter++
+            }
+        } else if (start > end) { // if 1st input is > than 2nd input
+            for (j = end; j <= start+1; j++) {
+                total += counter;
+                counter++;
+            }
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
