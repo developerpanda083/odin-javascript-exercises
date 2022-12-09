2a8b0f76f650db14f5574143874fd9a4d5b0b188
developerpanda083 <cpandalee083@gmail.com>
2022-12-09 16:54:49 -0500

    Exercise 07 finished and passed. Commented

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// converts farenheit to celsius, rounded to 1 decimal place
+const convertToCelsius = function(tempF) {
+  let Faren = (5/9) * (tempF - 32);
+  let roundFaren = Math.round(Faren * 10) / 10;
+  return roundFaren
 };
 
-const convertToFahrenheit = function() {
+// converts celsius to farenheit, rounded to 1 decimal place
+const convertToFahrenheit = function(tempC) {
+  let Celc = ((9/5) * tempC) + 32;
+  let roundCelc = Math.round(Celc * 10) / 10;
+  return roundCelc
 };
 
 // Do not edit below this line
